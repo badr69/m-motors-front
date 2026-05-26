0ad90c11436d7c43f55d3a0319022f5b383ae023
badreddine <manoudb@yahoo.fr>
2026-05-26 22:44:21 +0200

    fix api.js

--- static/js/api.js
+++ static/js/api.js
@@ -1,12 +1,13 @@
-const isLocal =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1";
+// const isLocal =
+//     window.location.hostname === "localhost" ||
+//     window.location.hostname === "127.0.0.1";
 
-const API_BASE = isLocal
-    ? "http://127.0.0.1:5001/api/v1"
-    : "http://84.46.241.76:5000/api/v1";
+// const API_BASE = isLocal
+//     ? "http://127.0.0.1:5001/api/v1"
+//     : "http://84.46.241.76:5000/api/v1";
 // const API_BASE = "/api/v1";
-// const API_BASE = "/api/v1";
+const API_BASE = "http://localhost:5001/api/v1";
+
 
 function getToken() {
     return localStorage.getItem("token");
