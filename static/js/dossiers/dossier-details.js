e38723090ab9dcfdb9cb0c12fb8d5d4e7bde4e36
badreddine <manoudb@yahoo.fr>
2026-06-21 09:19:17 +0200

    fix url_image

--- static/js/dossiers/dossier-details.js
+++ static/js/dossiers/dossier-details.js
@@ -69,6 +69,8 @@ async function loadDossier() {
     // ======================
     // DOSSIER INFO
     // ======================
+    alert(imageUrl);
+    console.log("IMAGE URL =", imageUrl);
     infoBox.innerHTML = `
 
         <div class="mb-3 text-center">
