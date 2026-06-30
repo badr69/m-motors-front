b469ee814ce61a69f15dbc5d392ad96a0dc3a87c
badreddine <manoudb@yahoo.fr>
2026-06-30 16:20:13 +0200

    change url_base to https://m-motors.duckdns.org/api/v1

--- static/js/api.js
+++ static/js/api.js
@@ -6,13 +6,12 @@ const isLocal =
 
 const API_BASE = isLocal
     ? "http://127.0.0.1:5001/api/v1"
-    : "http://84.46.241.76:5001/api/v1";
+    : "https://m-motors.duckdns.org/api/v1";
 
 const FILE_BASE = isLocal
     ? "http://127.0.0.1:5001"
-    : "http://84.46.241.76:5001";
+    : "https://m-motors.duckdns.org";
 
-    
 
 function getToken() {
     return localStorage.getItem("token");
